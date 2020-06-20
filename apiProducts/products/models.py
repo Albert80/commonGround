7e39f62a77e8e401ddf3139af90818b94c17ce0a
César Alberto Trejo <cesaratj27@gmail.com>
2020-06-19 21:25:48 -0500

    Se define el modelo Products con sus validadores

--- apiProducts/products/models.py
+++ apiProducts/products/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 
@@ -5,7 +6,7 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 class Product(models.Model):
   id = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4)
   name = models.CharField(max_length=55, validators=[MinLengthValidator(4), MaxLengthValidator(55)])
-  value = models.DecimalField(max_digits=5, decimal_places=1)
+  value = models.DecimalField(max_digits=6, decimal_places=1)
   discount_value= models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)
   stock = models.PositiveIntegerField(default=0)
   createdAt = models.DateTimeField(auto_now_add=True)
