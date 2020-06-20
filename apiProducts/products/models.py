c45c18308b70cb0000be95c549d15aed5cd6ca01
César Alberto Trejo <cesaratj27@gmail.com>
2020-06-19 21:10:19 -0500

    Se crea el modelo Product

--- apiProducts/products/models.py
+++ apiProducts/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
+class Product(models.Model):
+  id = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4)
+  name = models.CharField(max_length=55, validators=[MinLengthValidator(4), MaxLengthValidator(55)])
+  value = models.DecimalField(max_digits=5, decimal_places=1)
+  discount_value= models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)
+  stock = models.PositiveIntegerField(default=0)
+  createdAt = models.DateTimeField(auto_now_add=True)
+  updatedAt = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.name
