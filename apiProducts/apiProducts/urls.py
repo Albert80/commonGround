1dd5e1c2f12e236a5306c2305fd3972d834461cf
César Alberto Trejo <cesaratj27@gmail.com>
2020-06-23 10:11:22 -0500

    Se definen las urls asociados con ProductAPI + GET api/products + POST api/products/bulk_insert

--- apiProducts/apiProducts/urls.py
+++ apiProducts/apiProducts/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from products import views
+# from products import views
+from products import api
 
 urlpatterns = [
-    path('api/products', views.products_process),
-    path('api/products/bulk_insert', views.products_process),
+    # path('api/products', views.products_process),
+    # path('api/products/bulk_insert', views.products_process),
+    path('api/products', api.ProductAPI.as_view({'get': 'list'})),
+    path('api/products/bulk_insert', api.ProductAPI.as_view({'post': 'create'})),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
