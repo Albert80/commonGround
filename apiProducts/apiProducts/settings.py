7fc680a035f8c6b0f47556fa679d7c07a9627eb8
César Alberto Trejo <cesaratj27@gmail.com>
2020-06-23 21:04:15 -0500

    Se actualizan las configuraciones para elastic

--- apiProducts/apiProducts/settings.py
+++ apiProducts/apiProducts/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static'
 
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
